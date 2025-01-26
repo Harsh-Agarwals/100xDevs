cc5a5af5d721c8ef51dc0e61bf38df0d5505593f
Harsh Agarwal <harshag.code@gmail.com>
2025-01-27 01:27:45 +0530

    'logic-done'

---
+++ basics/quiz-app/script.js
@@ -0,0 +1,240 @@
+const getJson = async () => {
+    let res = await fetch("content.json");
+    let qns = await res.json();
+    return qns;
+}
+
+const questions = await getJson();
+
+const totalQuestions = questions.length;
+
+let questionNumber = document.getElementsByClassName("question-number")[0];
+let question = document.getElementsByClassName("question-desc")[0];
+let options = document.querySelectorAll(".options button");
+
+const questionIndex = document.getElementById("question-status");
+
+let answers = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
+let correctAnswers = [];
+
+questions.forEach(qn => {
+    let correctAnswer = qn["answer"];
+    let answerIndex = qn["options"].indexOf(correctAnswer)+1;
+    correctAnswers.push(answerIndex);
+})
+
+console.log(correctAnswers);
+
+
+let next = document.querySelector(".next");
+let prev = document.querySelector(".prev");
+let clear = document.querySelector(".clear");
+let confirm = document.querySelector(".confirm");
+
+let submit = document.querySelector(".submit");
+
+submit.addEventListener("click", res => {
+    let correct = 0;
+    for (let i=0;i<answers.length;i++) {
+        if (answers[i] == correctAnswers[i]) {
+            correct += 1;
+        }
+    }
+    let percentCorrect = correct*10;
+    console.log(answers, percentCorrect);
+})
+
+const colorStatus = () => {
+    let nums = document.querySelectorAll(".question-status li");
+    let i=0;
+    nums.forEach(num => {
+        if (answers[i] >= 1) {
+            num.style.backgroundColor = "lightgreen";
+        } else {
+            num.style.backgroundColor = "lightcyan";
+        }
+        i+=1;
+    })
+}
+colorStatus();
+
+prev.style.display = "none";
+
+options.forEach(btn => {
+    btn.addEventListener("click", e => {
+        options.forEach(btn => {
+            btn.classList.remove("select");
+        })
+        btn.classList.toggle("select");
+    })
+})
+
+confirm.addEventListener("click", e => {
+    let i=0;
+    let empty = true;
+    let current = document.querySelector(".question-number").textContent;
+    let currentIndex = current.indexOf('-');
+    let currentQuestion = parseInt(current.slice(currentIndex+1));
+    options = document.querySelectorAll(".options button");
+
+    options.forEach(btn => {
+        if (btn.classList.contains("select")) {
+            answers[currentQuestion - 1] = i+1;
+            empty = false;
+        }
+        i+=1;
+    })
+    if (empty == true) {
+        answers[currentQuestion - 1] = 0;
+    }
+    colorStatus();
+})
+
+clear.addEventListener("click", e => {
+    options = document.querySelectorAll(".options button");
+
+    options.forEach(btn => {
+        if (btn.classList.contains("select")) {
+            btn.classList.remove("select");
+        }
+        btn.style.backgroundColor = "rgb(238, 251, 251)";
+    })
+})
+
+prev.addEventListener("click", e => {
+    let current = document.querySelector(".question-number").textContent;
+    let currentIndex = current.indexOf('-');
+    let currentQuestion = parseInt(current.slice(currentIndex+1));
+
+    let newQuestion = questions[currentQuestion-2];
+    
+    questionNumber.textContent = `Question-${currentQuestion-1}`;
+    question.textContent = newQuestion.question;
+    let optionIndex = 0;
+    options.forEach(opt => {
+        opt.textContent = newQuestion["options"][optionIndex];
+        optionIndex+=1;
+    })
+
+    if (currentQuestion-1 == 1) {
+        prev.style.display = "none";
+        next.style.display = "inline";
+    } else {
+        prev.style.display = "inline";
+        next.style.display = "inline";
+    }
+
+    let answer = answers[currentQuestion - 2];
+    let optionIndex2 = 0;
+    options.forEach(btn => {
+        if (btn.classList.contains("select")) {
+            btn.classList.remove("select");
+        }
+        btn.style.backgroundColor = "rgb(238, 251, 251)";
+        if (answer != 0 & optionIndex2+1==answer) {
+            btn.classList.add("select");
+        }
+        optionIndex2+=1;
+    })
+
+    options.forEach(btn => {
+        btn.addEventListener("click", e => {
+            options.forEach(btn => {
+                btn.classList.remove("select");
+            })
+            btn.classList.toggle("select");
+        })
+    })
+})
+
+next.addEventListener("click", e => {
+    let current = document.querySelector(".question-number").textContent;
+    let currentIndex = current.indexOf('-');
+    let currentQuestion = parseInt(current.slice(currentIndex+1));
+
+    let newQuestion = questions[currentQuestion];
+    
+    questionNumber.textContent = `Question-${currentQuestion+1}`;
+    question.textContent = newQuestion.question;
+    let optionIndex = 0;
+    options.forEach(opt => {
+        opt.textContent = newQuestion["options"][optionIndex];
+        optionIndex+=1;
+    })
+
+    if  (currentQuestion+1 == 10) {
+        next.style.display = "none";
+        prev.style.display = "inline"
+    } else {
+        prev.style.display = "inline";
+        next.style.display = "inline";
+    }
+
+    let answer = answers[currentQuestion];
+    let optionIndex2 = 0;
+    options.forEach(btn => {
+        if (btn.classList.contains("select")) {
+            btn.classList.remove("select");
+        }
+        btn.style.backgroundColor = "rgb(238, 251, 251)";
+        if (answer != 0 & optionIndex2+1==answer) {
+            btn.classList.add("select");
+        }
+        optionIndex2+=1;
+    })
+
+    options.forEach(btn => {
+        btn.addEventListener("click", e => {
+            options.forEach(btn => {
+                btn.classList.remove("select");
+            })
+            btn.classList.toggle("select");
+        })
+    })
+})
+
+questionIndex.addEventListener("click", e => {
+    let questionAsked = parseInt(e.target.textContent);
+    let newQuestion = questions[questionAsked-1]; 
+    
+    questionNumber.textContent = `Question-${questionAsked}`;
+    question.textContent = newQuestion.question;
+    let optionIndex = 0;
+    options.forEach(opt => {
+        opt.textContent = newQuestion["options"][optionIndex];
+        optionIndex+=1;
+    })
+
+    if (questionAsked == 1) {
+        prev.style.display = "none";
+        next.style.display = "inline";
+    } else if  (questionAsked == 10) {
+        next.style.display = "none";
+        prev.style.display = "inline"
+    } else {
+        prev.style.display = "inline";
+        next.style.display = "inline";
+    }
+
+    let answer = answers[questionAsked - 1];
+    let optionIndex2 = 0;
+    options.forEach(btn => {
+        if (btn.classList.contains("select")) {
+            btn.classList.remove("select");
+        }
+        btn.style.backgroundColor = "rgb(238, 251, 251)";
+        if (answer != 0 & optionIndex2+1==answer) {
+            btn.classList.add("select");
+        }
+        optionIndex2+=1;
+    })
+
+    options.forEach(btn => {
+        btn.addEventListener("click", e => {
+            options.forEach(btn => {
+                btn.classList.remove("select");
+            })
+            btn.classList.toggle("select");
+        })
+    })
+})
