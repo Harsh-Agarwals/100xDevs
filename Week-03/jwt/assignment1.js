7e252b548d94a9c0f9c358079c9a37417db6dab1
Harsh Agarwal <harshag.code@gmail.com>
2025-02-17 23:11:01 +0530

    'auth

---
+++ Week-03/jwt/assignment1.js
@@ -0,0 +1,24 @@
+const { log } = require("console");
+const jwt = require("jsonwebtoken");
+const z = require("zod");
+
+function zodCheck(username, password) {
+  const nameSchema = z.string().email();
+  const pwdSchema = z.string().min(6);
+  return nameSchema.safeParse(username).success && pwdSchema.safeParse(password).success;
+}
+
+function getJwt(username, password) {
+  const check = zodCheck(username, password);
+  console.log(check);
+  if (!check) return null;
+  const token = jwt.sign({username}, 'harsh-harsh', {expiresIn: '1h'});
+  return token;
+}
+
+const res1 = getJwt("Harsh", 'ashy1gh');
+const res2 = getJwt("Harsh@gmail.com", '14nb');
+const res3 = getJwt("Harsh@gmail.com", '14nb1bb#');
+
+console.log(res1, res2, res3);
+
