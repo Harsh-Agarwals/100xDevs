2d564052c69fc8bec5380aae34e252bd139dd8cb
Harsh Agarwal <harshag.code@gmail.com>
2026-04-03 13:42:03 +0530

    few-more-week-15

---
+++ nextjs/landing-page/app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1 className=" font-bold text-4xl text-transparent bg-gradient-to-l bg-clip-text from-green-400 via-yellow-400 to-blue-500">Hello, harsh!</h1>
+    </div>
+  );
+}
